9aa661a179e3e9491ca06012d5955507529e7779
AlumaJoe <chinedualuma@outlook.com>
2023-09-16 08:48:10 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: valued 0
+ */
+
+int main()
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
